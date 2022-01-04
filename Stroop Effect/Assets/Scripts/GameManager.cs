1b260853bec6fb74b527dc76821711be1b91843b
RhysNorton <rbroughsmyth@gmail.com>
2022-01-04 14:02:49 +1030

    Added a bool to prevent colour names from repeating

--- Stroop Effect/Assets/Scripts/GameManager.cs
+++ Stroop Effect/Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
 
     [SerializeField, Tooltip("The amount of options that will be instantiated")]
     private int optionAmount = 4;
+    [SerializeField, Tooltip("Whether multiple of the same option are allowed to exist at once")]
     private bool repeatColours = true;
 
     [Space]
@@ -65,8 +66,13 @@ public class GameManager : MonoBehaviour
         colours.Remove(targetColour);
         colours.Add(targetColour);
 
-        //Assigns a random colour name to the target text field except for the last element in the list
-        targetText.text = colours[RandomIndex(colours, true)].name;
+        //Creates a list containing all colour names besides the target colour name
+        List<string> colourNames = new List<string>();
+        for (int i = 0; i < colours.Count - 1; i++)
+            colourNames.Add(colours[i].name);
+
+        //Assigns a random colour name to the target text field besides the target colour name
+        targetText.text = colourNames[RandomIndex(colourNames)];
 
         //Initialises a list of text fields for the names of each option's colour
         List<TextMeshProUGUI> optionTexts = new List<TextMeshProUGUI>();
@@ -76,9 +82,9 @@ public class GameManager : MonoBehaviour
             optionTexts.Add(Instantiate(optionPrefab, optionGroup).GetComponentInChildren<TextMeshProUGUI>());
 
         //Forgoes each option from having a different colours if there aren't enough
-        if (colours.Count < optionAmount)
+        if (!repeatColours && colours.Count < optionAmount)
         {
-            repeatColours = false;
+            repeatColours = true;
             Debug.LogWarning("There are less colours than there are options");
         }
 
@@ -107,7 +113,10 @@ public class GameManager : MonoBehaviour
                 optionButton.onClick.AddListener(IncorrectColour);
 
                 //Sets the option text to any colour's name besides the target's
-                optionTexts[currentOptionIndex].text = colours[RandomIndex(colours, true)].name;
+                optionTexts[currentOptionIndex].text = colourNames[RandomIndex(colourNames)];
+
+                //Prevents colour names from repeating among the options
+                if (!repeatColours) colourNames.Remove(optionTexts[currentOptionIndex].text);
             }
 
             //Removes the option so it can't have its name and colour value assigned again
@@ -138,15 +147,9 @@ public class GameManager : MonoBehaviour
     /// </summary>
     /// <typeparam name="T">The type used in the list</typeparam>
     /// <param name="list">The list from which a random index will be returned</param>
-    /// <param name="removeLastIndex">prevents the index of the last element to be returned</param>
     /// <returns></returns>
-    private int RandomIndex<T>(List<T> list, bool removeLastIndex = false)
+    private int RandomIndex<T>(List<T> list)
     {
-        int max;
-
-        if (removeLastIndex) max = list.Count - 1;
-        else max = list.Count;
-
-        return Random.Range(0, max);
+        return Random.Range(0, list.Count);
     }
 }
